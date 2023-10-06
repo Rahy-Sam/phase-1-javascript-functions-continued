243d5ea2b948e60c44110c76f7126f079bfcc5d6
Raymond <raymond.mwaiki@student.moringaschool.com>
2023-10-06 17:11:59 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    console.log(`I love to ${activity}`)
+}
+saturdayFun("Mountain climb")
+
+function mondayWork(whatIdo="go to the office"){
+    console.log(`I usually ${whatIdo} on Monday`)
+}
+mondayWork("work from home")
+
+function wrapAdjective(adjective="special"){
+const innerfunction= function(inneradjective="you are valued"){
+console.log `You are ${adjective} where ${inneradjective}` 
+}
+return inneradjective= "you are amazing"
+}
+adjective="great"
